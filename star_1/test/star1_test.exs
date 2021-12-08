1c2989bd9a24bd9dbf248a961b94158bb055752b
mariomelo <mariofdemelo@gmail.com>
2021-12-08 12:40:46 -0300

    Moving old solutions to livebook

--- star_1/test/star1_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Star1Test do
-  use ExUnit.Case
-  doctest Star1
-
-  test "greets the world" do
-    assert Star1.hello() == :world
-  end
-end
--- star_2/test/star2_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Star2Test do
-  use ExUnit.Case
-  doctest Star2
-
-  test "greets the world" do
-    assert Star2.hello() == :world
-  end
-end
